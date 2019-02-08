2689f6ba6324844bfd043734d86bb00409fc0716
KyoshinSan <jonathan.dinh@ynov.com>
2019-02-08 09:03:54 +0100

    Update create_database.sql

--- create_database.sql
+++ create_database.sql
@@ -1,11 +1,37 @@
 CREATE DATABASE IF NOT EXISTS DINH_HERNANDEZ;
+
 use DINH_HERNANDEZ;
 
--- Create table of exemple
-CREATE TABLE IF NOT EXISTS DINH_HERNANDEZ
+
+CREATE TABLE IF NOT EXISTS user
 (
     id INT NOT NULL AUTO_INCREMENT,
-    uid VARCHAR(255) NOT NULL,
-    url VARCHAR(1000) NOT NULL,
-    PRIMARY KEY (id)
+    email VARCHAR(150) NOT NULL UNIQUE,
+    password VARCHAR(150) NOT NULL,
+    api_key VARCHAR(150) NOT NULL UNIQUE,
+    PRIMARY KEY (id),
+    UNIQUE(email),
+    UNIQUE(api_key)
 );
+
+
+CREATE TABLE IF NOT EXISTS url
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    url VARCHAR(150) NOT NULL UNIQUE,
+    name VARCHAR(150) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE(url)
+);
+
+INSERT INTO user 
+(
+  email, 
+  password, 
+  api_key
+) VALUES (
+  'deschaussettes@yopmail.com',
+  'password',
+  'abcdefghjaimelesapis'
+);
+
