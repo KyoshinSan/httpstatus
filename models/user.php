630e26c33a2b2f425df75701e17825eceb92c8c4
KyoshinSan <jonathan.dinh@ynov.com>
2019-02-10 22:45:42 +0100

    fix

--- models/user.php
+++ models/user.php
@@ -4,12 +4,12 @@ namespace models;
 
 class user extends \Model
 {
-    public function get_one_by_email ($email)
+    public function get_one_by_email (string $email)
     {   
         return $this->get_one('user', ['email' => $email]);
     }   
     
-    public function get_one_by_api_key ($api_key)
+    public function get_one_by_api_key (string $api_key)
     {   
         return $this->get_one('user', ['api_key' => $api_key]);
     }   
