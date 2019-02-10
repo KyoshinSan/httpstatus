eca12687b0f2d3def44ee75a2d8f88f542ef4e93
KyoshinSan <jonathan.dinh@ynov.com>
2019-02-10 21:38:35 +0100

    Update HTML

--- templates/httpstatus/home.php
+++ templates/httpstatus/home.php
@@ -29,6 +29,48 @@
   </div>
 </div>
 
+<!-- Header -->
+<header class="w3-display-container w3-content w3-wide" style="max-width:1500px;" id="home">
+  <img class="w3-image" src="http://cloud-docker.com/wp-content/uploads/2012/11/cropped-wallpaper-1425819.jpg" alt="Architecture" width="1500" height="800">
+  <div class="w3-display-middle w3-margin-top w3-center">
+    <h1 class="w3-xxlarge w3-text-white"><span class="w3-padding w3-red w3-opacity-min"><b>HTTP</b></span> <span class="w3-hide-small w3-text-light-grey">Status</span></h1>
+  </div>
+</header>
+
+<!-- Page content -->
+<div class="w3-content w3-padding" style="max-width:1564px">
+
+<!-- Contact Section -->
+  <div class="w3-container w3-padding-32" id="websites">
+    <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Liste des sitewebs :</h3>
+    <p>Voici la liste des serveurs que nous surveillons.</p>
+    <table class="w3-table w3-bordered">
+        <tr>
+          <th>État du site</th>
+          <th>Nom du site</th>
+          <th>URL</th>
+        </tr>
+        <tr>
+          <td>Site UP</td>
+          <td>Google</td>
+          <td>https://www.google.fr/</td>
+        </tr>
+        <tr>
+          <td>Site DOWN</td>
+          <td>Facebook</td>
+          <td>https://www.facebook.com</td>
+        </tr>
+        <tr>
+          <td>Site UP</td>
+          <td>Toto</td>
+          <td>https://www.toto.fr</td>
+        </tr>
+      </table>
+  </div>
+
+<!-- End page content -->
+</div>
+
 <!-- Connexion Fenêtre -->
 <div id="connexion" class="w3-modal">
     <div class="w3-modal-content w3-animate-zoom" style="padding:32px">
--- templates/incs/footer.php
+++ templates/incs/footer.php
@@ -1,2 +1,6 @@
+	<footer class="w3-center w3-black w3-padding-16">
+  		<p>Created by Jonathan DINH & Theo HERNANDEZ</p>
+	</footer>
+
 	</body>
 </html>
